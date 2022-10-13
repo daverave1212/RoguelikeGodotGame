a6d884a77be7c176d9159869476de99f92b5197a
Georgi Kanchev <52318254+Dodo-Codes@users.noreply.github.com>
2022-10-13 03:24:09 +0300

    Fixed bandit prefab
    - The bandit prefab is now a prefab variant of the unit template prefab, rather than a copy
    - The bandit's behavior shuts off once dead

--- Scripts/Units/DumbBanditAIController.cs
+++ Scripts/Units/DumbBanditAIController.cs
@@ -1,10 +1,8 @@
-using System;
-
 using Godot;
 
 public class DumbBanditAIController : UnitAIController
 {
-	bool IsMoving = false;
+	bool IsMoving, IsShooting;
 	Vector2 MoveDirection;
 
 	public override void _Ready()
@@ -14,31 +12,49 @@ public class DumbBanditAIController : UnitAIController
 		Delay.DoEvery(1.5f, TryShoot);
 		GD.Print($"My tag: {MyUnit.Tag}");
 	}
-	
+
 	void MoveRandomly()
 	{
+		if(MyUnit.IsDead)
+		{
+			Delay.Cancel(MoveRandomly);
+			return;
+		}
+
 		var dirX = Utils.RandomFloat(-1, 1);
 		var dirY = Utils.RandomFloat(-1, 1);
 		MoveDirection = new Vector2(dirX, dirY);
 		IsMoving = true;
-		Delay.DoAfter(1f, () =>
-		{
-			IsMoving = false;
-		});
+		Delay.DoAfter(1f, () => IsMoving = false);
 	}
 	void TryShoot()
 	{
-		if (MyUnit.IsMoving)
+		if(MyUnit.IsMoving)
 			return;
+
+		if(MyUnit.IsDead)
+		{
+			Delay.Cancel(TryShoot);
+			return;
+		}
+
 		var targetPlayerUnit = MyUnit.AcquireTarget();
-		if (targetPlayerUnit == null) return;
-		MyUnit.PlayAnimation("Attack");
+		if(targetPlayerUnit == null)
+			return;
+
+		IsShooting = true;
+		Delay.DoAfter(0.5f, () =>
+		{
+			MyUnit.PlayAnimation("Idle");
+			IsShooting = false;
+		});
 		MyUnit.ShootWeaponAt(targetPlayerUnit);
+		MyUnit.PlayAnimation("Attack");
 	}
 
 	public override void _PhysicsProcess(float delta)
 	{
-		if (IsMoving)
+		if(IsMoving && IsShooting == false && MyUnit.IsDead == false)
 			MyUnit.MoveInDirection(MoveDirection);
 	}
 }
